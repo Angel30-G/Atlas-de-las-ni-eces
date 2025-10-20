6e50058954d245d8225904fb2ca86c4b9e35a087
Angel <gabrielvargas493@gamil.com>
2025-10-20 11:35:41 -0600

    Se agrega un mapa nuevo

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -5,7 +5,7 @@ import Principes from "@/modules/start/components/Principes";
 import Areas from "@/modules/start/components/Areas";
 import Footer from "@/components/Footer";
 import Gallery from "@/modules/gallery-maps/Gallery"
-import MapHome from "@/modules/home-map";
+import MapHome from "@/modules/home-map/MapHome";
 import { Stack } from "@mui/material";
 
 export default function Inicio() {
@@ -13,9 +13,9 @@ export default function Inicio() {
   return (
     <>
       <Hero />
-       <MapHome
+      <MapHome
                 text="Toda la información que los niños y niñas de 4° grado nos compartieron es ahora parte del mapa digital libre de OpenStreetMap, explora el mapa y las zonas de interes"
-                mapLink="//umap.openstreetmap.fr/pt-br/map/mapa-alajuela-por-nineces_991953?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"
+                mapLink="https://umap.openstreetmap.fr/es/map/mapa-sin-titulo_1300328?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true&datalayers=d8ccf1d8-87ce-4afd-9717-f722dc93f9e4#16/10.0158/-84.2132"
               />
       <Areas />
       <Footer />
